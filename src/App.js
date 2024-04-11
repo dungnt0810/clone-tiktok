26858270c43c5ad33788e183987b28251fae1fe6
dungnt <dung.nguyen.tien.0810@gmail.com>
2024-04-12 00:04:15 +0700

    build: add routers and layouts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes, privateRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
+
 function App() {
     return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                    ;
+                    {privateRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={Page} />;
+                    })}
+                    ;
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
