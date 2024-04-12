6b1b6884863577f5415515d71afb1071ba50f92e
dungnt <dung.nguyen.tien.0810@gmail.com>
2024-04-12 22:37:16 +0700

    feat: build DefaultLayout frame

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,17 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+console.log(cx('wrapper'));
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <h2>Header</h2>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
