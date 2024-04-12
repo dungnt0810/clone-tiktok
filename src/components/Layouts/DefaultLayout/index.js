c9760cb860315eec4368661583fd7e6013ec8a86
dungnt <dung.nguyen.tien.0810@gmail.com>
2024-04-12 21:48:10 +0700

    refactor: change header to component and rename Layouts

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,10 +1,12 @@
-import Header from './Header';
+import Header from '../components/Header';
+import Sidebar from './Sidebar';
 
 function DefaultLayout({ children }) {
     return (
         <div>
             <Header />
             <div className="container">
+                <Sidebar />
                 <div className="content">{children}</div>
             </div>
         </div>
